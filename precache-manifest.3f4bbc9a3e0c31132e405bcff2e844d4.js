cf9dc763b3e7aa2cb2fc2bfd474f37036b18c2c2
Arman Alizadeh <armanalizadeh467@gmail.com>
2020-05-22 12:27:10 +0200

    Updates

---
+++ precache-manifest.3f4bbc9a3e0c31132e405bcff2e844d4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4810164692e4944f97e5c20ef20a9c3b",
+    "url": "/sneakers/index.html"
+  },
+  {
+    "revision": "cf7cb6ffa8c51a3a78fd",
+    "url": "/sneakers/static/css/main.42fc3ed4.chunk.css"
+  },
+  {
+    "revision": "631ece856425a0a7278f",
+    "url": "/sneakers/static/js/2.fe5018e3.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/sneakers/static/js/2.fe5018e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf7cb6ffa8c51a3a78fd",
+    "url": "/sneakers/static/js/main.6233ded5.chunk.js"
+  },
+  {
+    "revision": "6e2a4a60ad29123f37f3",
+    "url": "/sneakers/static/js/runtime-main.1572f8f2.js"
+  },
+  {
+    "revision": "f2ac5a8ebd7e47fa53edfcdb73a116d8",
+    "url": "/sneakers/static/media/Running-Shoes-PNG-Image.f2ac5a8e.png"
+  },
+  {
+    "revision": "e9a430d2d2b3fb650fe32259db1bb36c",
+    "url": "/sneakers/static/media/blue1.e9a430d2.png"
+  },
+  {
+    "revision": "7c3b0ddf97b26efa3002f87fcc98e2fc",
+    "url": "/sneakers/static/media/blue2.7c3b0ddf.png"
+  },
+  {
+    "revision": "835982fe6182dbd7980712c4fad623a4",
+    "url": "/sneakers/static/media/blue3.835982fe.png"
+  },
+  {
+    "revision": "f94375dbb17291d0b1354b356c3f300b",
+    "url": "/sneakers/static/media/blue4.f94375db.png"
+  },
+  {
+    "revision": "f1335684bdaf32d5c75e98e9ef4a0364",
+    "url": "/sneakers/static/media/chevron-small-down.f1335684.svg"
+  },
+  {
+    "revision": "e0764d6a826e192b3c29884178d59c7f",
+    "url": "/sneakers/static/media/green1.e0764d6a.png"
+  },
+  {
+    "revision": "14f6981184feedfb324a02bdb4a74c1a",
+    "url": "/sneakers/static/media/green2.14f69811.png"
+  },
+  {
+    "revision": "28ea67433bb4f7cdc92a2f10a72c24ef",
+    "url": "/sneakers/static/media/green3.28ea6743.png"
+  },
+  {
+    "revision": "386d857512a524f5acb9f690185f30d1",
+    "url": "/sneakers/static/media/green4.386d8575.png"
+  },
+  {
+    "revision": "0dd0a3a7ccaed8a7bacf4ed3614d4226",
+    "url": "/sneakers/static/media/logo.0dd0a3a7.png"
+  },
+  {
+    "revision": "1fc2f6e80d8227c2672c2f3a5339fe39",
+    "url": "/sneakers/static/media/magenta1.1fc2f6e8.png"
+  },
+  {
+    "revision": "bbfae2584eba71fb8b26b38e0d90fe3e",
+    "url": "/sneakers/static/media/magenta2.bbfae258.png"
+  },
+  {
+    "revision": "95ee308756047cc5bfdf504559c7740d",
+    "url": "/sneakers/static/media/magenta3.95ee3087.png"
+  },
+  {
+    "revision": "0e39f68acccf79be55a614d57a8da21f",
+    "url": "/sneakers/static/media/magenta4.0e39f68a.png"
+  },
+  {
+    "revision": "eeaddb84906d587b0c9c5caebaef4055",
+    "url": "/sneakers/static/media/pink1.eeaddb84.png"
+  },
+  {
+    "revision": "347aac64d775a98a5d08d070b65eaa4d",
+    "url": "/sneakers/static/media/pink2.347aac64.png"
+  },
+  {
+    "revision": "00ba50e99166f71f083bdae918bc9f87",
+    "url": "/sneakers/static/media/pink3.00ba50e9.png"
+  },
+  {
+    "revision": "f00e58ec27dafff8abc61c6474441966",
+    "url": "/sneakers/static/media/pink4.f00e58ec.png"
+  },
+  {
+    "revision": "b38e9410b1fc34278d7a94d4c91bb100",
+    "url": "/sneakers/static/media/sprite.b38e9410.svg"
+  },
+  {
+    "revision": "0bb53a66be1062f2355689cdebf63a5d",
+    "url": "/sneakers/static/media/violet1.0bb53a66.png"
+  },
+  {
+    "revision": "ae8b1e0b259d456e495ebf81d54a6607",
+    "url": "/sneakers/static/media/violet2.ae8b1e0b.png"
+  },
+  {
+    "revision": "6b3d7371d34aea22d3480682355c3cd6",
+    "url": "/sneakers/static/media/violet3.6b3d7371.png"
+  },
+  {
+    "revision": "5f250143c45c0263f689cd22b32feadd",
+    "url": "/sneakers/static/media/violet4.5f250143.png"
+  }
+]);
